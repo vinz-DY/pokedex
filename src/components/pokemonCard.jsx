3053df8f078124f60e432b6a153f920e8ad0e1ab
vinz-DY <vincentducamp259@hotmail.com>
2023-10-10 17:21:02 +0200

    solution challenge quete

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,10 +1,27 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-  return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-     <figcaption>Bulbasaur</figcaption>
-      </figure>
-  );
+    const pokemon = pokemonList[0];
+    const imageok = pokemon.imgSrc ?(
+        <img src={pokemon.imgSrc} alt={pokemon.name}/>
+    ):(<p>???</p>
+    );
+
+    return (
+        <div>
+            <div>{pokemon.name}</div>
+            <div>{imageok}</div>
+        </div>
+    );
 }
 
 
