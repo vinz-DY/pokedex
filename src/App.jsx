d8fa9120cb2ff8a614eb1e97b1c86b2c34ba61c4
vinz-DY <vincentducamp259@hotmail.com>
2023-10-11 12:09:49 +0200

    solution quete props ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import PokemonCard from "./components/pokemonCard";
+
+// ------------ici mes const ---------------------------
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+// -----------------ici mes fonctions --------------------
+
 function Card() {
   return (
     <div>
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemonList[0]} />
    </div>
   );
 }
--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,28 +1,18 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCard() {
-    const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
+
     const imageok = pokemon.imgSrc ?(
         <img src={pokemon.imgSrc} alt={pokemon.name}/>
     ):(<p>???</p>
     );
 
     return (
-        <div>
+        <>
             <div>{pokemon.name}</div>
             <div>{imageok}</div>
-        </div>
-    );
-}
+        </>
+  );
 
+}
 
 export default PokemonCard;
